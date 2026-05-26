490d7bdd366f59ffe12f3c3a257dc88c90b926c4
Matthew Westcott <matthew@west.co.tt>
2026-05-26 13:31:50 +0100

    Revert "Upgrade to Django 5.2"
    This reverts commit ed3c2d6111c958593b9208acb2968596bd9a7f11.

--- productions/tests/test_production_views.py
+++ productions/tests/test_production_views.py
@@ -581,7 +581,7 @@ class TestEditCoreDetails(TestCase):
             response,
             (
                 '<input type="hidden" name="form-0-party_party_id" class="party_field_party_id" '
-                'aria-describedby="id_form-0-party_error" id="id_form-0-party" aria-invalid="true" value="%d">'
+                'id="id_form-0-party" aria-invalid="true" value="%d">'
             )
             % Party.objects.get(name="Forever 2e3").id,
             html=True,
